47f3842d7c1d10a1bad52a8ba6e41e8f6ec34b60
JLEscobar <160857638+JLEscobar@users.noreply.github.com>
2024-02-22 08:12:06 -0500

    Update voce.py

--- voce.py
+++ voce.py
@@ -1,1 +1,9 @@
+import streamlit as st
+import gTTS as gtts
+from PIL import Image
+st.title("Cambio texto a voz")
+
+st.header("En este espacio comiezo a desarrollar mis aplicaciones para interfaces multimodales")
+st.write("facilmente pudo realizar banked y fronted")
+image = Image.open("Avatar_belial.png")
 
